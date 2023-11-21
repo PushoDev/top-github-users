90137caa5aeb09d2938154afd08b4e64c8652ed3
Pusho.Dev <bethocubans1990@gmail.com>
2023-11-21 10:07:40 -0400

    Add files
    Agregar otras configuraciones

---
+++ index.js
@@ -0,0 +1,12 @@
+// Proceso de Top Usuarios Cubanos
+// Trabajado por Luis A. Guisado (PushoDev)
+
+
+    // const AUTH_KEY = "ghp_vbmFdybMFCxWzvrgC*************";
+    // const GITHUB_USERNAME_AND_REPOSITORY = 'gayanvoice/top-github-users';
+
+
+    // const AUTH_KEY = process.env.CUSTOM_TOKEN;
+    // const GITHUB_USERNAME_AND_REPOSITORY = process.env.GITHUB_REPOSITORY;
+
+// e-mail: bethocubans1990@gmail.com
